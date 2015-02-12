cdf2a30f564f4518accecc59cfe365085e7aa5d1
rayray1 <rupiray@yahoo.com>
2015-02-12 19:31:24 +0000

    sqlite

--- sqlb.py
+++ sqlb.py
@@ -1,4 +1,4 @@
-# INSERT Command
+# INSERT Command with Error Handler
 
 
 # import the sqlite3 library
@@ -12,14 +12,17 @@ conn = sqlite3.connect("new.db")
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
 
+try:
+	# insert data
+	cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 5000000)")
+	cursor.execute("INSERT INTO population VALUES('San Fransisco', 'CA', 800000)")
 
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 5000000)")
-cursor.execute("INSERT INTO population VALUES('San Fransisco', 'CA', 800000)")
 
+	# commit the changes
+	conn.commit()
 
-# commit the changes
-conn.commit()
+except sqlite3.OperationalError:
+	print "OOps! Something went wrong. Try Again..."
 
 
 # close the database connection
